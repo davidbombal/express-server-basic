619f40cacc03fec9767fe76b80bb35cc425a5767
David Bombal <davidbombal@gmail.com>
2022-08-03 13:35:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 
 # We load the newline seperated commands from the commands file.
 with open("commands.txt", "r") as fh:
-# We drop the empty entries using the filter() function.
+# We use the readlines() method to get a list of all the lines in the file.
     cmds = fh.readlines()
     for item in cmds:
 # We run each command using the subprocess.run() function in Python.
